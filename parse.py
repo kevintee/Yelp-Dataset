df1138d0a12179de19d8d6061279889c596b4528
kevintee <kevintee@berkeley.edu>
2014-04-24 12:05:44 -0700

    Fix bug with ' token during parsing

--- parse.py
+++ parse.py
@@ -49,7 +49,8 @@ def create_combined_review_data_set(review_file_name):
         feature_vector.append(blob.sentiment.polarity)
         feature_vector.append(blob.sentiment.subjectivity)
 
-        words = [x.singularize() for x in blob.lower().words]
+
+        words = [x.singularize() for x in blob.lower().split(' ')]
         # TODO: add features of selected word counts
         #       need to do some processing to figure out which words matter
 
