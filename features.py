1be5bca1b1d0b6f11c99e032b17f706a35391085
kevintee <kevintee@berkeley.edu>
2014-04-26 14:20:57 -0700

    Working implementation of scoring function

--- features.py
+++ features.py
@@ -10,6 +10,7 @@ class FeatureGenerator(object):
             blob = TextBlob(datum['text'])
             self.blobs.append(blob)
             self.stars.append(int(datum['stars']))
+        self.common_words = [self._get_star_words(x) for x in range(1, 6)]
 
     def get_blob(self, idx):
         return self.blobs[idx]
@@ -44,8 +45,17 @@ class FeatureGenerator(object):
                 all_caps_count += 1
         return all_caps_count
 
+    def generate_similarity_between_words(self, idx, num_stars):
+        words = self.common_words[num_stars-1]
+        score = 0
+        for word in self.blobs[idx].words:
+            if word in words:
+                score += words[word]
+        return score
+
     def _get_star_words(self, num_stars):
         words = {}
+        stop = stopwords.words('english')
         for star, blob in zip(self.stars, self.blobs):
             if star == num_stars:
                 for word in blob.words:
@@ -56,6 +66,7 @@ class FeatureGenerator(object):
                         words[word] = 1
         words = sorted(words.items(), key=lambda x: x[1])
         words.reverse()
-        stop = stopwords.words('english')
-        words = [x for x in words if x[0] not in stop and "'" not in x[0]]
-        return words
+        words = [x for x in words if x[0] not in stop]
+        words = [x for x in words if "'" not in x[0]]
+        words = words[:50]
+        return dict(words)
